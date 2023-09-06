0ebbb1a39a2845dd17522669941ed22390c53125
MD KAMRUL HASAN <114093612+chasmkhasan@users.noreply.github.com>
2023-09-06 20:06:06 +0200

    first commit

---
+++ ForumASPMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ForumASPMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
